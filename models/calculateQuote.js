e4577197c0d6af406f02b9153bba844519cf9f37
samsonhumber <printerfissh693@gmail.com>
2022-09-02 11:27:22 +0100

    set up model for quotes

---
+++ models/calculateQuote.js
@@ -0,0 +1,17 @@
+export function calculateQuote(age, breed, address, numberPets) {
+    // Number Pets given by the size array
+    const basePrice = 120;
+    // If noPets more thean too, call that function multipetdiscount
+    // When telling if breed is valid, then apply discuont there
+}
+
+function multiPetDiscount(price) {
+    const discount = 0.9;
+    return discount*price 
+}
+
+function breedDiscount(price, breed) {
+    const discountedBreeds = ['germanshepherd', 'pug', 'boxer'];
+    const discount = 0.9;
+    return discount*numberPets
+}
