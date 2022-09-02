b629838e5ffe257cce9323707e6e7745b65ef41c
ghavasi <g.havasi@outlook.com>
2022-09-02 15:28:55 +0100

    Update counter.js

--- public/counter.js
+++ public/counter.js
@@ -4,7 +4,7 @@ let counter = document.querySelector(".count");
 async function getCount() {
   const response = await fetch("http://localhost:3000/quotes");
   const display = response;
-  counter.innerHTML = display.payload.count;
+  counter.innerHTML = display[0].count;
 }
 
 getCount();
